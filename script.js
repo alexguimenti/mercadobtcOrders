f6b2400ec08b8e5a89d22dab0917da744cc95999
mexSocialMiner <alexandre.oliveira@socialminer.com>
2021-03-12 15:20:11 -0300

    update

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-setTimeout(script, 1500);
+console.log('Start Interval!')
+var loading = setInterval(waiting, 300);
+
+function waiting() {
+  if(document.querySelector('[data-el="orderbook"]')){
+    clearTimeout(loading)
+    console.log("Found!")
+  }
+}
+
+setTimeout(script, 3000);
 
 function script() {
   var lines = [];
@@ -14,7 +24,16 @@ function script() {
     document.querySelectorAll('.order-resume-info').forEach(line => {
       if (line.querySelector('td').className.includes('type-buy')) {
         var price = line.querySelector('.value-buy').textContent.substr(3).replace('.', '').replace(',', '.')
-        var amount = line.querySelector('.value-qtd').textContent.slice(0, -5).replace(',', '.');
+        var amount = line.querySelector('.value-qtd').textContent.replace('.', '').slice(0, -5).replace(',', '.');
+        var total = price * amount;
+        total = total.toFixed(2)
+        // console.log(price, amount, total);
+        // console.log(line.querySelector('.status > span').innerHTML)
+        line.querySelector('.status > span').innerHTML = `R$ ${total}`
+      }
+      if (line.querySelector('td').className.includes('type-sell')) {
+        var price = line.querySelector('.value-sell').textContent.substr(3).replace('.', '').replace(',', '.')
+        var amount = line.querySelector('.value-qtd').textContent.replace('.', '').slice(0, -4).replace(',', '.');
         var total = price * amount;
         total = total.toFixed(2)
         console.log(price, amount, total);
